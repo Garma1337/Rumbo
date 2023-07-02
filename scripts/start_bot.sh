f43b9773527cc39e28013fc6a148ee9aeda61b5e
garma <garma@some.domain.com>
2023-07-02 19:11:35 +0200

    fixed start_bot.sh script

--- scripts/start_bot.sh
+++ scripts/start_bot.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 CURRENT_DIRECTORY=$(dirname "$0")
-VENV_BIN="${CURRENT_DIRECTORY}/venv/bin/activate"
-BOT_BIN="${CURRENT_DIRECTORY}/bot.py"
+VENV_BIN="${CURRENT_DIRECTORY}/../venv/bin/activate"
+BOT_BIN="${CURRENT_DIRECTORY}/../bot.py"
 
 source "$VENV_BIN"
 python3 -B "$BOT_BIN"
